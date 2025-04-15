e046335215846ef93b025de01a220b2267180acc
Elsie <elsieoscar06@gmail.com>
2025-04-15 20:03:41 +0300

    routing code along

--- src/routes.js
+++ src/routes.js
@@ -7,24 +7,31 @@ import ErrorPage from "./pages/ErrorPage";
 const routes = [
   {
     path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+          {
+    path: "/",
     element: <Home />,
-    errorElement: <ErrorPage />
+    
   }, 
   {
     path: "/about",
     element: <About />,
-    errorElement: <ErrorPage />
+    
   },
   {
     path: "/login",
     element: <Login />,
-    errorElement: <ErrorPage />
+    
   },
   {
     path: "/profile/:id",
     element: <UserProfile />,
-    errorElement: <ErrorPage />
+    
   }
+]
+}
 ];
 
 export default routes;
